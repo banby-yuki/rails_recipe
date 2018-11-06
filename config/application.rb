0cdf81c05e7b6a400c0e24186205364aa44c2177
banby-yuki <30365186+banby-yuki@users.noreply.github.com>
2018-11-06 10:45:23 +0900

    second
    recipe tuturial

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Recipes
+module Myrecipes
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.action_cable.mount_path = '/cable'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_103621) do
+ActiveRecord::Schema.define(version: 20170217191829) do
 
   create_table "chefs", force: :cascade do |t|
-    t.string "chefname"
-    t.string "email"
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean  "like"
+    t.integer  "chef_id"
+    t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "chef_id"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "chef_id"
+    t.string   "image"
   end
 
 end
